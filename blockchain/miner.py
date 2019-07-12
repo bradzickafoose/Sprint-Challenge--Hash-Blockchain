c4d72848d284a26e9c7140a2da027f1fb106e106
briandoyle81 <briandoyle81@gmail.com>
2019-07-11 18:01:29 -0600

    spelling fix

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -32,7 +32,7 @@ def proof_of_work(last_proof):
 def valid_proof(last_hash, proof):
     """
     Validates the Proof:  Multi-ouroborus:  Do the last six characters of
-    the last hash matchc the first six characters of the proof?
+    the last hash match the first six characters of the proof?
 
     IE:  last_hash: ...999123456, new hash 123456888...
     """
