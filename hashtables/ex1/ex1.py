d430f06cfb5db24479059e38d5c554b730fce459
Brad <brad@viziworx.com>
2020-04-10 13:00:11 -0400

    add function to get indices of item weights and removes unused functions

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,17 +1,22 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    # find the pair of item weights thats sum equals the weight limit and store in the hash table
+    for high_value_index in range(length):
+      weight = weights[high_value_index]
+      low_value_index = hash_table_retrieve(ht, limit - weight)
+      if low_value_index is None:
+        hash_table_insert(ht, weight, high_value_index)
+      # return the item weights of the two packages that equals the weight limit
+      else:
+        answer = (high_value_index, low_value_index)
+        return answer
 
     return None
 
